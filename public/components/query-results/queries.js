3e252760929e639b944d4e787e4eca50248ae903
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-06-15 13:32:41 -0600

    Refactored so bm-art-piece has its own controller

--- public/components/query-results/queries.js
+++ public/components/query-results/queries.js
@@ -1,5 +1,5 @@
 angular.module("mainApp")
-    .controller("queryCtrl", ["$scope", "bmService", "favService", "imgService", function ($scope, bmService, favService, imgService) {
+    .controller("queryCtrl", ["$scope", "bmService", function ($scope, bmService) {
         $scope.categories = ["Open Playa", "Mobile"];
         $scope.bmArtPieces = [];
         $scope.getBmApi = function (art) {
@@ -18,35 +18,4 @@ angular.module("mainApp")
                     }
                 );
         };
-        $scope.addFav = function (artPiece) {
-            var imgConfig = {
-                params: {
-                    ref: artPiece.images[0].gallery_ref
-                }
-            };
-            var imgUrl = "";
-            imgService.getList(imgConfig)
-                .then(
-                    function (response) {
-                        var n = response.search("http://galleries.burningman.org/filestore");
-                        imgUrl = response.slice(n, n + 96);
-                        artPiece.imageUrls = [imgUrl];
-                        var newImg = artPiece.images[0].thumbnail_url;
-                        if (artPiece.year === 2017) {
-                            artPiece.imageUrls = [newImg];
-                        }
-
-                        return favService.postFav(artPiece)
-                    }
-                ).then(
-                function (response) {
-                    return response
-                },
-                function (response) {
-                    alert("There was an error:\n" + response.data.errmsg);
-                    console.log(response.status);
-                    console.log(response)
-                }
-            )
-        }
     }]);
--- public/directives/bm-art-piece/bmArtPiece.js
+++ public/directives/bm-art-piece/bmArtPiece.js
@@ -5,6 +5,41 @@ angular.module("mainApp")
             restrict: "E",
             scope: {
                 piece: "=",
-            }
+            },
+            controller: "bmArtPieceCtrl"
         }
-    });
+    })
+
+    .controller("bmArtPieceCtrl", ["$scope", "bmService", "imgService", "favService", function ($scope, bmService, imgService, favService) {
+        $scope.addFav = function (artPiece) {
+            var imgConfig = {
+                params: {
+                    ref: artPiece.images[0].gallery_ref
+                }
+            };
+            var imgUrl = "";
+            imgService.getList(imgConfig)
+                .then(
+                    function (response) {
+                        var n = response.search("http://galleries.burningman.org/filestore");
+                        imgUrl = response.slice(n, n + 96);
+                        artPiece.imageUrls = [imgUrl];
+                        var newImg = artPiece.images[0].thumbnail_url;
+                        if (artPiece.year === 2017) {
+                            artPiece.imageUrls = [newImg];
+                        }
+
+                        return favService.postFav(artPiece)
+                    }
+                ).then(
+                function (response) {
+                    return response
+                },
+                function (response) {
+                    alert("There was an error:\n" + response.data.errmsg);
+                    console.log(response.status);
+                    console.log(response)
+                }
+            )
+        }
+    }]);
