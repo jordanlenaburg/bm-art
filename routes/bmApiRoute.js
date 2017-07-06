512e1222742c866c74a095c52d51ad76fc3d05e7
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-07-06 08:04:04 -0600

    added config file, moved auth key

--- routes/bmApiRoute.js
+++ routes/bmApiRoute.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var request = require("request");
 var bmRoute = express.Router();
+var config = require("../config");
 bmRoute.get("/", function (req, res) {
     var url = "https://api.burningman.org/api/v1/art";
     request.get({url:url, qs:req.query}, function (err, response, body) {
@@ -8,7 +9,7 @@ bmRoute.get("/", function (req, res) {
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // console.log('body:', body);
         res.send(body)
-    }).auth("c02cd0af512b03a31adfd96274d740f1", "");
+    }).auth(config.authKey, "");
 });
 bmRoute.get("/:id", function (req, res) {
     request.get("https://api.burningman.org/api/v1/art/" + req.params.id, function (err, response, body) {
@@ -16,6 +17,6 @@ bmRoute.get("/:id", function (req, res) {
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         // console.log('body:', body);
         res.send(body)
-    }).auth("c02cd0af512b03a31adfd96274d740f1", "");
+    }).auth(config.authKey, "");
 });
 module.exports = bmRoute;
